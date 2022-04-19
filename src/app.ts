9e2bd06f2df4cc02593bfc64d2d5c972de3d9349
Muhammad Rizky Firdaus <rizkyfirdaus0309@gmail.com>
2022-04-19 14:35:33 +0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { 
+  Express, 
+  Request, 
+  Response, 
+} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res : Response) => {
+  res.send('Express + Typescript server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
